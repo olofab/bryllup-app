eb4fc87fef0cc3199a8221ba4df0cd65b75ed9d8
Andreas Bergman <andreasbergman93@gmail.com>
2022-04-19 20:55:42 +0200

    initial commit for forside

---
+++ src/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+
+const StyledFooter = styled.footer`
+    margin: 1rem;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1rem;
+    text-align: center;
+`;
+export default function Footer() {
+    return (
+        <StyledFooter>
+                Har dere spørsmål ta kontakt med Anne Karoline på 11223344 eller Philip på 11223344
+        </StyledFooter>
+    );
+}
+
---
+++ src/Infomation.tsx
@@ -0,0 +1,107 @@
+import React, {ReactComponentElement} from 'react';
+import './App.css';
+import styled from 'styled-components';
+import {ReactComponent as Wedding} from "./icons/wedding.svg";
+import {ReactComponent as Party} from "./icons/party.svg";
+import {ReactComponent as Rings} from "./icons/rings.svg";
+
+
+
+const StyledTop = styled.header`
+    display: flex;
+    justify-content: center;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1rem;
+       
+    @media(max-width: 800px) {
+        display: flex;
+        flex-direction: column;
+    }
+`;
+const StyledSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-grow:1;
+    
+ 
+
+`;
+
+const Wrapper = styled.div`
+    background-color: rgba(211,211,211,0.2);
+    padding: 2rem;
+
+`;
+
+const Heading = styled.div`
+    text-align: center;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 2rem;
+    padding: 3rem 0rem 2rem 0rem;
+
+`;
+
+const Title = styled.div`
+    text-align: center;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1.5rem;
+    margin-top: 0.5rem;
+`;
+
+const Description = styled.p`
+    text-align: center;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1rem;
+
+`;
+
+interface Props {
+    title: string;
+    time?: string;
+    description: string;
+    icon: ReactComponentElement<any>;
+}
+
+const ChurchIcon = styled(Wedding)`
+    height: 20%;
+    width: 20%;
+`;
+
+const PartyIcon = styled(Party)`
+    height: 20%;
+    width: 20%;
+`;
+
+const RingIcon = styled(Rings)`
+    color: black;
+    height: 20%;
+    width: 20%;
+`;
+
+
+const InfomationComponent = (props : Props) => {
+    return (
+        <StyledSection>
+            {props.icon}
+            <Title>{props.title}</Title>
+            <Description>{props.time}</Description>
+            <Description>{props.description}</Description>
+        </StyledSection>
+    )
+}
+
+export default function Infomation() {
+    return (
+        <Wrapper>
+            <Heading>Seremoni og feiring</Heading>
+            <StyledTop>
+                <InfomationComponent title={"Vår bryllupsdag"} time={"Lørdag 24 juni 2023"} description={"Her kommer det en litt lengre tekst om hvor man skal møte opp og viktig info"} icon={<RingIcon/>}/>
+                <InfomationComponent title={"Sermoni"} time={"Oppmøte kl 13:00"} description={"Her kommer det en litt lengre tekst om hvor man skal møte opp og viktig info"} icon={<ChurchIcon/>}/>
+                <InfomationComponent title={"Middag"} time={"Skåtøy Kafe klokken 17:00"} description={"Her kommer det en litt lengre tekst om hvor man skal møte opp og viktig info"} icon={<PartyIcon/>}/>
+            </StyledTop>
+        </Wrapper>
+
+    );
+}
+
